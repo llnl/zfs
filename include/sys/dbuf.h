bd32176ad916102408ed47d397a3226866a22cef
Olaf P. Faaland <faaland1@llnl.gov>
2022-04-28 08:37:29 -0700

    TOSS-5638: Increase dbuf cache size
    When running an HPSS repack (sending data from our old disk cache to our new
    disk cache) to our new ASP hardware HPSS admins and developers noticed a HUGE
    amount of CPU usage on our systems.
    
    They found that /proc/spl/kstat/zfs/dbufstats was reporting hash_chain_max of
    13.
    
    HPSS uses zvols, whereas our Lustre systems do not.  Possibly this is due to
    less uniform hashing of the blocks comprising zvols than non-zvol blocks, but
    this has not been verified.
    
    Increasing the size of the hash table dramatically improved performance,
    resulting in has_chain_max of 2.
    
    The hash table size was reduced recently, possibly too much.  See
    https://github.com/openzfs/zfs/pull/12289
    
    An upstream patch will need to be written and landed, possibly to simply
    increase the dbuf cache size, or to dynamically size this hash, but this
    mitigation is sufficient for us in the meantime.  Our hardware typically has
    plenty of RAM.
    
    Patch courtesy of Brian Behlendorf and Herb Wartens.

--- include/sys/dbuf.h
+++ include/sys/dbuf.h
@@ -322,7 +322,7 @@ typedef struct dmu_buf_impl {
 } dmu_buf_impl_t;
 
 /* Note: the dbuf hash table is exposed only for the mdb module */
-#define	DBUF_MUTEXES 2048
+#define	DBUF_MUTEXES 32768
 #define	DBUF_HASH_MUTEX(h, idx) (&(h)->hash_mutexes[(idx) & (DBUF_MUTEXES-1)])
 typedef struct dbuf_hash_table {
 	uint64_t hash_table_mask;
